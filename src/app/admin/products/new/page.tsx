35a933fd946c99f371abf016a2b4796a1bc95dbb
Varuzh21 <varuzhan.grigoryan@urartian.am>
2024-07-24 18:03:26 +0400

    add

---
+++ src/app/admin/products/new/page.tsx
@@ -0,0 +1,12 @@
+import {PageHeader} from "@/app/admin/_components/PageHeader";
+import {ProductForm} from "@/app/admin/products/_components/ProductForm";
+
+export default function NewProductPage() {
+    return (
+        <>
+            <PageHeader>Add Product</PageHeader>
+            <ProductForm />
+        </>
+    )
+
+}
